a6879625c95ce59c2a7b768050fe6fcbabad8e7a
abhipin <abhinavpin@gmail.com>
2022-03-21 16:07:52 +0530

    initial file upload

---
+++ responsive.js
@@ -0,0 +1,8 @@
+
+
+if (window.innerWidth < 1374) {
+    const rsmbCollection = document.getElementsByClassName("no-show")
+    for (v of rsmbCollection) {
+        v.style.display = 'none';
+    }
+}
